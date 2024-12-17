cbdb5c0890be944e6a9896c4f402d2674c47bd23
Syakir Rahman <syakirurahman@gmail.com>
2024-12-17 07:50:28 +0700

    initialized slickpress-lite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import { loadEnv } from "vite";
+
+const { SITE } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), "");
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE,
+  env: {
+    schema: {
+      SITE: envField.string({ context: "server", access: "public", default: 'http://localhost:4321' })
+    }
+  },
+  integrations: [tailwind(), partytown(), sitemap()]
+});
